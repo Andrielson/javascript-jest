387ef2c167f981fb370796b7702566429b5be174
Andrielson Silva <andrielson@gmail.com>
2022-02-28 14:16:19 -0300

    Add queryString methods and their tests

--- project/src/lib/queryString.spec.js
+++ project/src/lib/queryString.spec.js
@@ -3,11 +3,34 @@ const { queryString } = require('./queryString');
 describe('Object to query string', () => {
   it('should create a valid query string when an object is provided', () => {
     const obj = {
-      name: 'Fabio',
+      name: 'Andrielson',
       profession: 'developer',
     };
 
-    expect(queryString(obj)).toBe('name=Fabio&profession=developer');
+    expect(queryString(obj)).toBe('name=Andrielson&profession=developer');
+  });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Andrielson',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Andrielson&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Andrielson',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
   });
 });
 
